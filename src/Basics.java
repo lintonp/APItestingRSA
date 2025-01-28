ab7928972b2bfe19c4f90c4f5b42d96ccf34e267
lintonp <pereira.linton@gmail.com>
2025-01-28 08:27:37 +0530

    API response assertions with body payload being imported

--- src/Basics.java
+++ src/Basics.java
@@ -1,6 +1,7 @@
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
+import files.Payload;
 import io.restassured.RestAssured;
 
 public class Basics {
@@ -9,22 +10,7 @@ public class Basics {
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
 		given().log().all().queryParams("key", "qaclick123")
 		.header("Content-Type", "application/json")
-		.body("{\r\n"
-				+ "  \"location\": {\r\n"
-				+ "    \"lat\": -38.383494,\r\n"
-				+ "    \"lng\": 33.427362\r\n"
-				+ "  },\r\n"
-				+ "  \"accuracy\": 50,\r\n"
-				+ "  \"name\": \"Frontline house\",\r\n"
-				+ "  \"phone_number\": \"(+91) 983 893 3937\",\r\n"
-				+ "  \"address\": \"29, side layout, cohen 09\",\r\n"
-				+ "  \"types\": [\r\n"
-				+ "    \"shoe park\",\r\n"
-				+ "    \"shop\"\r\n"
-				+ "  ],\r\n"
-				+ "  \"website\": \"http://google.com\",\r\n"
-				+ "  \"language\": \"French-IN\"\r\n"
-				+ "}")
+		.body(Payload.AddPlaceBody())
 		.when().post("/maps/api/place/add/json")
 		.then().log().all().assertThat().statusCode(200)
 				.body("scope", equalTo("APP"))
